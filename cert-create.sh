8e233caff54edc3d8beb76452fa31fb318395e4f
Daniel Parkes <dparkes@redhat.com>
2024-08-22 18:03:08 +0200

    Update cert-create.sh

--- cert-create.sh
+++ cert-create.sh
@@ -4,41 +4,41 @@ USER_HOME=$(cat /etc/passwd | grep ^U | cut -d: -f6)
 wget https://gist.githubusercontent.com/likid0/f034a5e8472b1ed38370463e8b06cc93/raw/9f49c677390493b2e8bdc4e768b563fcf13f2326/helperfile.adoc -O $USER_HOME/cli-helper-1518.adoc
 chmod 644 $USER_HOME/cli-helper-1518.adoc
 
-PROFILE_DIR=$(find $HOME/.mozilla/firefox -type d -name "*.default*" | head -n 1)
-
-if [ -z "$PROFILE_DIR" ]; then
-    echo "No Firefox profile found. Starting Firefox to create one..."
-    firefox & sleep 5 && pkill firefox
-fi
+## Configure html cli helper file as the default Firefox home page
+#PROFILE_DIR=$(find $HOME/.mozilla/firefox -type d -name "*.default*" | head -n 1)
+#if [ -z "$PROFILE_DIR" ]; then
+#    echo "No Firefox profile found. Starting Firefox to create one..."
+#    firefox & sleep 5 && pkill firefox
+#fi
 
 # Search for the prefs.js file in the user's home directory
-PREFS_PATH=$(find $HOME -type f -name "prefs.js" | grep ".mozilla/firefox" | head -n 1)
+#PREFS_PATH=$(find $HOME -type f -name "prefs.js" | grep ".mozilla/firefox" | head -n 1)
 
 # Check if the prefs.js file was found
-if [ -z "$PREFS_PATH" ]; then
-    echo "prefs.js file not found in the home directory."
-    exit 1
-fi
+#if [ -z "$PREFS_PATH" ]; then
+#    echo "prefs.js file not found in the home directory."
+#    exit 1
+#fi
 
-# Define the path to the local HTML file
-HTML_FILE_PATH="$HOME/cli-helper-1518.html"
+## Define the path to the local HTML file
+#HTML_FILE_PATH="$HOME/cli-helper-1518.html"
 
-# Convert the file path to a URL format
-FILE_URL="file://$HTML_FILE_PATH"
+## Convert the file path to a URL format
+#FILE_URL="file://$HTML_FILE_PATH"
 
-# Backup the current prefs.js file
-cp "$PREFS_PATH" "$PREFS_PATH.bak"
+## Backup the current prefs.js file
+#cp "$PREFS_PATH" "$PREFS_PATH.bak"
 
-# Check if the user.js file exists in the same directory as prefs.js and create it if not
-USER_JS_PATH=$(dirname "$PREFS_PATH")/user.js
-if [ ! -f "$USER_JS_PATH" ]; then
-    touch "$USER_JS_PATH"
-fi
+## Check if the user.js file exists in the same directory as prefs.js and create it if not
+#USER_JS_PATH=$(dirname "$PREFS_PATH")/user.js
+#if [ ! -f "$USER_JS_PATH" ]; then
+#    touch "$USER_JS_PATH"
+#fi
 
-# Add the local file URL to the startup pages (home page) in user.js
-echo 'user_pref("browser.startup.homepage", "'$FILE_URL'");' >> "$USER_JS_PATH"
+## Add the local file URL to the startup pages (home page) in user.js
+# echo 'user_pref("browser.startup.homepage", "'$FILE_URL'");' >> "$USER_JS_PATH"
 
-echo "Firefox will open with $FILE_URL on startup."
+# echo "Firefox will open with $FILE_URL on startup."
 
 
 # Create Self-Signed Certs for LAB
